dc04ceba3e8e5836b1c943258606c2cdc1618bc6
Zhifan Lan <everydaysiling@gmail.com>
2020-01-01 18:38:29 -0700

    longest

---
+++ Leetcode/Top 100 Liked Questions/3-Longest Substring Without Repeating Characters/Longest.1.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int Longest(String s) {
+        int maxLength = 0;
+        int slideWindowStartPointer = 0;
+        int slideWindowEndPointer = 0;
+        HashMap<Character, Boolean> isExisted = new HashMap<Character, Boolean>();
+        /* Generic Collection classes cant be used with primitives */
+        for (int i = 0; i < s.length(); i++) {
+            char m = s.charAt(i);
+            if (isExisted.get(m) == null) {
+                isExisted.put(m, true);
+            } else if (isExisted.get(m) == true) {
+                for (int k = slideWindowStartPointer; k < slideWindowEndPointer; k++) {
+                    if (s.charAt(k) == m) {
+                        slideWindowStartPointer = k + 1;
+                    }
+                }
+            }
+            slideWindowEndPointer++;
+            maxLength = Math.max(maxLength, (slideWindowEndPointer - slideWindowStartPointer));
+        }
+        return maxLength;
+    }
+}
+
+/*
+ * Key point: using sliding window to calculate the maximum length
+ */
