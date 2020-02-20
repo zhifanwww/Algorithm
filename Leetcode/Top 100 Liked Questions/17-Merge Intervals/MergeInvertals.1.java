e90e210675ec79f3efb784a4a1b3b6ac866fc893
Zhifan Lan <everydaysiling@gmail.com>
2020-02-19 23:08:28 -0700

    Merge interval

--- Leetcode/Top 100 Liked Questions/17-Merge Intervals/MergeInvertals.1.java
+++ Leetcode/Top 100 Liked Questions/17-Merge Intervals/MergeInvertals.1.java
@@ -4,7 +4,7 @@ class Solution {
     public int[][] merge(int[][] intervals) {
         ArrayList<int[]> result = new ArrayList<int[]>();
         Arrays.sort(intervals, (int[] interval1, int[] interval2) -> interval1[0] > interval2[0] ? 1
-                : interval1[0] < interval2[0] ? -1 : 0);
+                : interval1[0] < interval2[0] ? -1 : 0);// ?
         for (int[] interval : intervals) {
             int size = result.size();
             if (size == 0 || (result.get(size - 1)[1] < interval[0])) {
@@ -14,6 +14,6 @@ class Solution {
                         Math.max(result.get(size - 1)[1], interval[1]) });
             }
         }
-        return result.toArray(new int[result.size()][]);
+        return result.toArray(new int[result.size()][]);// ?
     }
 }
