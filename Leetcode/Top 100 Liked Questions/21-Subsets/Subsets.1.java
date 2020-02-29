83c4026f31724feeae5584dd9b90c31007b4b1c1
Zhifan Lan <everydaysiling@gmail.com>
2020-02-28 21:30:58 -0700

    subset

--- Leetcode/Top 100 Liked Questions/21-Subsets/Subsets.1.java
+++ Leetcode/Top 100 Liked Questions/21-Subsets/Subsets.1.java
@@ -1,5 +1,28 @@
 class Solution {
+    ArrayList ret = new ArrayList<>();
+    int targetLength;
+
     public List<List<Integer>> subsets(int[] nums) {
+        for (targetLength = 0; targetLength < nums.length + 1; targetLength++) {
+            backtracking(0, new ArrayList<>(), nums);
+        }
+        return ret;
+
+    }
+
+    private void backtracking(int add_pos, ArrayList curr, int[] nums) {
+        if (curr.size() == targetLength) {
+            ret.add(new ArrayList(curr));
+            return;
+        }
+
+        for (int i = add_pos; i < nums.length; i++) {
+            curr.add(nums[i]);
+            backtracking(i + 1, curr, nums);
+            curr.remove(curr.size() - 1);
+        }
 
     }
 }
+
+/* Classic backtracking soluion */
