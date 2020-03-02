9c2fbe0a07e4ff6dd3692877edc0b3459edcf05f
Zhifan Lan <everydaysiling@gmail.com>
2020-03-01 18:35:22 -0700

    word serach

---
+++ Leetcode/Top 100 Liked Questions/22-Word Search/WordSearch.1.java
@@ -0,0 +1,30 @@
+
+class Solution {
+    public boolean exist(char[][] board, String word) {
+
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                if (dfs(word, board, i, j) == true) {
+                    return true;
+                }
+            }
+        }
+        return false;
+
+    }
+
+    private boolean dfs(String word, char[][] board, int i, int j) {
+
+        if (word.length() == 0) {
+            return true;
+        } else if (i < 0 || i > board.length - 1 || j < 0 || j > board.length - 1 || board[i][j] != word.charAt(0)) {
+            return false;
+        } else {
+            return dfs(word.substring(1), board, i + 1, j) || dfs(word.substring(1), board, i, j + 1)
+                    || dfs(word.substring(1), board, i, j - 1);
+        }
+
+    }
+}
+
+/* Intuitive dfs solution */
