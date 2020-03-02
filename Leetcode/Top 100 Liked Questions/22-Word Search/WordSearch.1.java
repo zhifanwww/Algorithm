7914851066648b2202acd12db2dad71429574fff
Zhifan Lan <everydaysiling@gmail.com>
2020-03-01 19:03:55 -0700

    revise answer, bug fix

--- Leetcode/Top 100 Liked Questions/22-Word Search/WordSearch.1.java
+++ Leetcode/Top 100 Liked Questions/22-Word Search/WordSearch.1.java
@@ -1,4 +1,3 @@
-
 class Solution {
     public boolean exist(char[][] board, String word) {
 
@@ -17,11 +16,16 @@ class Solution {
 
         if (word.length() == 0) {
             return true;
-        } else if (i < 0 || i > board.length - 1 || j < 0 || j > board.length - 1 || board[i][j] != word.charAt(0)) {
+        } else if (i < 0 || i > board.length - 1 || j < 0 || j > board[0].length - 1 || board[i][j] != word.charAt(0)) {
             return false;
         } else {
-            return dfs(word.substring(1), board, i + 1, j) || dfs(word.substring(1), board, i, j + 1)
-                    || dfs(word.substring(1), board, i, j - 1);
+            char tmp = board[i][j];
+            board[i][j] = '#'; // Mark visited nodes!!!
+            boolean res = dfs(word.substring(1), board, i + 1, j) || dfs(word.substring(1), board, i, j + 1)
+                    || dfs(word.substring(1), board, i, j - 1) || dfs(word.substring(1), board, i - 1, j);
+            board[i][j] = tmp;
+            return res;
+
         }
 
     }
